39d4b17dcddb48cfb21aefbf537ec2b5077abefa
SebastianBuryy <sebastianbury2004@gmail.com>
2025-02-22 16:56:10 +0000

    Added bar chart component

---
+++ contex-ai/components/agent-ui/AgentChart.tsx
@@ -0,0 +1,4 @@
+import { AgentControlled } from "@/components/agent-ui/AgentController";
+import { BarChartComponent } from "@/components/ui/bar-chart";
+
+export const AgentChart = AgentControlled(BarChartComponent);
