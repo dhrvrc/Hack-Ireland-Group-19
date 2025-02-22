888059fcb41e1f88dafeefb5dac8beb87d020d12
Dhruv Ranajit Choudhury <dchoudhu@tcd.ie>
2025-02-22 20:24:43 +0000

    Added CORS Middleware configuration

--- contex-ai-prompt-generation-backend/core_api/core_api/settings.py
+++ contex-ai-prompt-generation-backend/core_api/core_api/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     "prompts.apps.PromptsConfig"
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -112,6 +114,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+
+# CORS settings
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Add your frontend URL here
+    "http://127.0.0.1:3000",
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
