beed414523a9c12171d961ad74089954e1a3b5b5
SebastianBuryy <sebastianbury2004@gmail.com>
2025-02-22 21:01:19 +0000

    Added Navbar component

---
+++ contex-ai/components/agent-ui/AgentNavbar.tsx
@@ -0,0 +1,4 @@
+import { AgentControlled } from "@/components/agent-ui/AgentController"
+import { NavigationMenuDemo } from "@/components/ui/navbar"
+
+export const AgentNavbar = AgentControlled(NavigationMenuDemo)
