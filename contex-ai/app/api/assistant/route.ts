71b4bb5f19ed937c4cd3bf3935a0773cc187a3c2
Cian McNamara <cian@telecomeireann.ie>
2025-02-23 05:05:44 +0000

    assistant api

---
+++ contex-ai/app/api/assistant/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const openai = new OpenAI();
+const assistant = getAssistant();
+const thread = await openai.beta.threads.create();
+
+export async function GET(req: Request) {
+  const message = await openai.beta.threads.messages.create(
+    thread.id,
+    {
+      role: "user",
+      content: "" + req.text
+    }
+  );
+
+  return NextResponse.json(message.content);
+}
+
+async function getAssistant() {
+    const assistant = await openai.beta.assistants.create({
+        name: "Contex",
+        instructions: "You are a software developer who creates components which are to be used by AI. Write your components and include appropriate AgentContext blocks around them.",
+        tools: [{ type: "code_interpreter" }],
+        model: "gpt-4o"
+      });
+
+    return assistant;
+}
